ed3747957ecbbac99676c3065f71096bf9721dde
Mohamadabosalh <mohamadabosalh7@gmail.com>
2025-04-09 21:56:39 +0300

    Initial commit

---
+++ JavaScript.js
@@ -0,0 +1,7 @@
+﻿<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  $(document).ready(function(){
+    $("#flip").click(function(){
+      $("#panel").slideToggle("slow");
+    });
+  });
+
